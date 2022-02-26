b6863cca52536b04305eae610041e6ae88ba9f30
William Sørensen <williamsorensen.work@gmail.com>
2022-02-26 21:32:47 +0100

    add: hello world frontend

---
+++ frontend/src/routes/index.svelte
@@ -0,0 +1,13 @@
+<script>
+	const string = 'hello world';
+</script>
+
+<nav class="header">
+	<b>Hello twitter</b>
+	<div><a href="/signin" class="sign-in">Sign in</a></div>
+</nav>
+
+<style lang="scss">
+	.header {
+	}
+</style>
