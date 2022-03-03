e7e5987da204738bce97b3cfed23a4daffa57d4c
William Sørensen <williamsorensen.work@gmail.com>
2022-03-04 00:01:48 +0100

    fix: cors

--- backend/src/routes.rs
+++ backend/src/routes.rs
@@ -1,8 +1,28 @@
 use rocket::{Build, Rocket};
 
+use crate::api::result::ApiResult;
+
 pub mod posts;
 pub mod user;
 
+#[catch(404)]
+fn options_handler(req: &rocket::Request<'_>) -> ApiResult<(), ()> {
+    let url = req.uri().to_string();
+
+    if req.method() == rocket::http::Method::Options {
+        ApiResult {
+            me: url,
+            data: None,
+            error: None,
+            refresh_token: None,
+        }
+    } else {
+        ApiResult::error(url, 404, ())
+    }
+}
+
 pub fn mount(rocket: Rocket<Build>) -> Rocket<Build> {
-    self::posts::mount(self::user::mount(rocket))
+    self::posts::mount(self::user::mount(
+        rocket.register("/", catchers![options_handler]),
+    ))
 }
