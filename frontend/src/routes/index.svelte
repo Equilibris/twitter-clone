5eb1298f92d63a1bbd0386d6df1948be9a7583a0
William Sørensen <williamsorensen.work@gmail.com>
2022-03-04 09:24:35 +0100

    feat: infinite scroll

--- frontend/src/routes/index.svelte
+++ frontend/src/routes/index.svelte
@@ -5,8 +5,10 @@
 	import type { Paths, PublicPost } from '$lib/typings/api';
 	import { get, post } from '$lib/utils/fetch';
 	import { me } from '$lib/data/me/store';
+	import { onMount } from 'svelte';
 
 	let feed: PublicPost[] = [];
+	let isFetching = true;
 
 	const get_results = async () => {
 		const results = await get<Paths['post']['feed']>(`/post/feed/${feed.length}`);
@@ -14,6 +16,7 @@
 		for (const result of results.data || []) if (result.data) feed.push(result.data);
 
 		feed = feed;
+		isFetching = false;
 
 		console.log(feed);
 	};
@@ -32,22 +35,63 @@
 			message = '';
 		}
 	};
+
+	let ioContainer: HTMLDivElement;
+
+	onMount(() => {
+		if (typeof IntersectionObserver !== 'undefined') {
+			const io = new IntersectionObserver(
+				async (v) => {
+					if (v[0].isIntersecting && !isFetching) {
+						isFetching = true;
+						await get_results();
+					}
+				},
+				{
+					rootMargin: '500px',
+					threshold: 0
+				}
+			);
+
+			io.observe(ioContainer);
+
+			return () => io.unobserve(ioContainer);
+		}
+	});
 </script>
 
 <Encapsulator>
-	{#if $me}
-		<form on:submit={handlePost}>
-			<textarea name="message" cols="30" rows="10" bind:value={message} />
-			<button>Post</button>
-		</form>
-	{/if}
+	<div class="container">
+		<div>
+			{#if $me}
+				<form on:submit={handlePost}>
+					<textarea name="message" cols="30" rows="10" bind:value={message} />
+					<button>Post</button>
+				</form>
+			{/if}
 
-	{#each feed as result}
-		<Post>
-			<svelte:fragment slot="author">
-				{result.author.data.name}
-			</svelte:fragment>
-			{result.message}
-		</Post>
-	{/each}
+			{#each feed as result}
+				<Post>
+					<svelte:fragment slot="author">
+						{result.author.data.name}
+					</svelte:fragment>
+					{result.message}
+				</Post>
+			{/each}
+			<div id="intersector" bind:this={ioContainer} />
+		</div>
+	</div>
 </Encapsulator>
+
+<style lang="scss">
+	form {
+		gap: 1em;
+		display: flex;
+		flex-direction: column;
+	}
+	.container {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+</style>
